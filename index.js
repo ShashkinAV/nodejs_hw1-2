dd57cf9da5ec764382dda11016c4bd4138946c63
Shashkin Anton <aveksis@gmail.com>
2020-03-28 12:49:53 +0300

    hw-2.1

--- index.js
+++ index.js
@@ -1,27 +1,164 @@
-const http = require('http');
-const port = 3000;
-const interval = process.argv[2] || 1000;
-const clear = process.argv[3] || 5000;
+const fs = require('fs');
+const path = require('path');
+const params = require('commander');
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-      let date;
-      let int = setInterval(() => {
-        date = new Date();
-        console.log(date.toUTCString());
-      }, interval);
-      
-      setTimeout(() => {
-        clearInterval(int);
-        res.write(`Time stop: ${date.toUTCString()} 
-                  Date: ${date.getFullYear()}-${date.getUTCMonth()+1}-${date.getUTCDate()}
-                  Time: ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}
-                  `);
-        res.end();
-      }, clear);  
-    } else {
-        res.end('No Get');
-    }   
+params
+  .option('-i, --in [in]', 'Input folder', './in')
+  .option('-o, --out [out]', 'Output folder', './out')
+  .option('-d, --delete', 'Delete input folder')
+  .parse(process.argv);
+
+const inDir = params.in;
+const outDir = params.out;
+const deleteInDir = params.delete;
+
+sortCollection(inDir, outDir, deleteInDir).then(() => {
+  console.log('DONE!');
+}).catch(err => {
+  console.log(err);
 });
 
-server.listen(port, () => console.log(`Srver running on port: ${port}`));
+function sortCollection (inDir, outDir, deleteInDir) {
+  return new Promise((resolve, reject) => {
+    (async () => {
+      try {
+        await checkAccess(inDir);
+      } catch (err) {
+        console.log('Input folder does not exist');
+        reject(err);
+      }
+
+      try {
+        await checkAccess(outDir);
+      } catch (err) {
+        try {
+          await createDir(outDir);
+        } catch (err) {
+          console.log('Can not create output folder');
+          reject(err);
+        }
+      }
+
+      try {
+        await sortDir(inDir);
+      } catch (err) {}
+
+      resolve();
+    })();
+  });
+}
+
+function checkAccess (path) {
+  return new Promise((resolve, reject) => {
+    fs.access(path, err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function createDir (path) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(path, err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function readDir (path) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path, (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(files);
+      }
+    });
+  });
+}
+
+function getStat (path) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (err, state) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(state);
+      }
+    });
+  });
+}
+
+function sortDir (dir) {
+  return new Promise((resolve, reject) => {
+    readDir(dir).then(files => {
+      Promise.all(files.map(item => {
+        return new Promise((resolve, reject) => {
+          const itemPath = path.join(dir, item);
+
+          getStat(itemPath).then(state => {
+            if (state.isFile()) {
+              const letter = item[0].toUpperCase();
+              const targetDir = path.join(outDir, letter);
+              const targetFile = path.join(targetDir, item);
+
+              checkAccess(targetDir).then(() => {
+                moveFile(item, itemPath, targetFile, targetDir, dir).then(resolve).catch(reject);
+              }).catch(() => {
+                createDir(targetDir).then(() => {
+                  moveFile(item, itemPath, targetFile, targetDir, dir).then(resolve).catch(reject);
+                }).catch(() => {
+                  moveFile(item, itemPath, targetFile, targetDir, dir).then(resolve).catch(reject);
+                });
+              });
+            }
+
+            if (state.isDirectory()) {
+              sortDir(itemPath).then(resolve).catch(reject);
+            }
+          }).catch(reject);
+        });
+      })).then(resolve).catch(reject);
+    }).catch(err => {
+      console.log('Can not read input folder');
+      reject(err);
+    });
+  });
+}
+
+function moveFile (item, itemPath, targetFile, targetDir, dir) {
+  return new Promise((resolve, reject) => {
+    let fileNameSuffix = 1;
+
+    while (fs.existsSync(targetFile)) {
+      const itemParts = path.parse(item);
+      targetFile = path.join(targetDir, itemParts.name + '_' + fileNameSuffix + itemParts.ext);
+      fileNameSuffix++;
+    }
+
+    fs.link(itemPath, targetFile, err => {
+      const result = err ? '[error]' : '[ok]';
+      console.log(itemPath + ' -> ' + targetFile + ' ' + result);
+
+      if (deleteInDir) {
+        fs.unlink(itemPath, () => {
+          fs.rmdir(dir, () => {});
+          fs.rmdir(inDir, () => {});
+        });
+      }
+
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
